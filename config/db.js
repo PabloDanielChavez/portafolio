d871af0e2f56de7edbd017aaee7c673c2c239cfe
--global <--global>
2024-12-14 16:07:11 -0300

    correccion de base de datos

--- config/db.js
+++ config/db.js
@@ -5,24 +5,28 @@ dotenv.config();
 
 console.log('Database URL:', process.env.DATABASE_URL); // Línea de depuración
 
-// if (!process.env.DATABASE_URL) {
-//     throw new Error("DATABASE_URL no está definido $env:DATABASE_URL # Para Windows PowerShellida en las variables de entorno");
-// }
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL no está definido $env:DATABASE_URL # Para Windows PowerShellida en las variables de entorno");
+}
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+import Sequelize from 'sequelize'
+import dotenv from 'dotenv';
+dotenv.config()
+ 
+const db = new Sequelize(process.env.DATABASE_URL, {
     define: {
         timestamps: false
     },
-    pool: {
-        max: 5,
-        min: 0,
+    pool:{
+        max :5,
+        min:0,
         acquire: 30000,
         idle: 10000
     },
-    operatorsAliases: false
+    operatorAliases: false
 });
+ 
+export default db;
 
 // Autenticar la conexión y manejar errores
 db.authenticate()
