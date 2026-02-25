c740e7aded533b3844426352e8978d009101ac02
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-02-25 07:52:53 -0300

    Maquetado y style de la seccion Header

--- portafolio-frontend/src/components/Header.tsx
+++ portafolio-frontend/src/components/Header.tsx
@@ -1,88 +1,24 @@
+
 "use client";
-import styles from '../styles/sections/header.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import styles_header from '../styles/sections/header.module.scss';
 
 export default function Header() {
-  const [scrolled, setScrolled] = useState(false);
-  const [activo, setActivo] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 50;
-      setScrolled(isScrolled);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Limpieza del evento al desmontar el componente
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-  
-
-    const manejarButton = () => {
-      setActivo(!activo); // Cambia el estado entre true y false
-    };
-
   return (
-    <header className={scrolled ? `${styles.header} ${styles.scrollOn}` : `${styles.header} ${styles.scrollOff}`}>
-      <button onClick={manejarButton} data-activo={activo} id={`buttonMenu`} className={`material-symbols-outlined ${styles.header__button}`}>
-        {activo ? 'close' : 'menu'}
-      </button>
-      <nav className={activo ? `${styles.menuActivado}` : `${styles.menuDesactivado}` }>
-          <ul className={styles.ventanaMenu}>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="http://localhost:3000">
-                Inicio
-                <span className='material-symbols-outlined'>home</span>
-              </a>
-            </li>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="http://localhost:3000/usuario/ingresar">
-                Ingresar
-                <span className='material-symbols-outlined'>person</span>
-              </a>
-            </li>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="http://localhost:3000/usuario/registro">
-                Registrarse
-                <span className='material-symbols-outlined'>person_add</span>
-              </a>
-            </li>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="http://localhost:3000/menu">
-                Menu
-                <span className='material-symbols-outlined'>restaurant</span>
-              </a>
-            </li>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="#">
-                Versiones  
-                <span className='material-symbols-outlined'>family_history</span>
-              </a>
-            </li>
-            <li>
-              <a  className={styles.ventanaMenu__btnNavOpc} href="http://localhost:3000/usuario/admin">
-                Admin  
-                <span className='material-symbols-outlined'>family_history</span>
-              </a>
-            </li>
-          </ul>
-      </nav>
-
-      <h1 id="header" className={styles.header__H1}>
-          P<span className={styles.header__H1Color}>ABLO CHAVEZ</span>
-        </h1>
-      <a
-        id="NmrTel"
-        className={styles.header__tel}
-        href="tel:+5491166621017"
-      >
-        +54 9 11 6662 1017
-      </a>
+    <header className={styles_header.header}>
+      <div className={styles_header.header_box}>
+        <div className={styles_header.header_layout}>
+          <article className={styles_header.header_box_available}>
+            <span className={styles_header.header_circle}></span>
+            <span className={styles_header.header_available}>Disponible para trabajar</span>
+          </article>
+          <article className={styles_header.header_box_time}>
+            <p className={styles_header.header_p_time}>Hora Local (ARG)</p>
+            <div className={styles_header.header_time}>
+              <span className={styles_header.header_span_time}>07:36:45</span>
+            </div>
+          </article>
+        </div>
+      </div>
     </header>
   );
 }
