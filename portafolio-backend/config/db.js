717975253d7e99755a446bd941170b542fb61a15
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-04-14 12:01:18 -0300

    Correccion de error, En DB a las variables de los buscaba como DB, pero en las variables de Render y local se los nombraba como BD. Error de tipeo :)

--- portafolio-backend/config/db.js
+++ portafolio-backend/config/db.js
@@ -1,12 +1,12 @@
 import Sequelize from 'sequelize';
 
 const db = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
+    process.env.BD_NAME, 
+    process.env.BD_USER, 
+    process.env.BD_PASS, 
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: process.env.BD_HOST,
+        port: process.env.BD_PORT,
         dialect: 'mysql',
         define: {
             timestamps: false
