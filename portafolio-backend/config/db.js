adb606757b05493f5d1e3b829a199250916ba784
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-04-14 11:24:09 -0300

    Nueva configuracion para la base de datos, DB con variable fija, pero ahora cuenta con la libreria cross/env la cual automatiza el consumo de ENV de development y production.

--- portafolio-backend/config/db.js
+++ portafolio-backend/config/db.js
@@ -1,22 +1,29 @@
-import Sequelize from 'sequelize'
+import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
-dotenv.config()
 
+dotenv.config();
 
-const db = new Sequelize(process.env.BD_LOCAL_NAME, process.env.BD_LOCAL_USER, process.env.BD_LOCAL_PASS,{
-    host: process.env.BD_LOCAL_HOST,
-    port: process.env.BD_LOCAL_PORT,
-    dialect: 'mysql',
-    define: {
-        timestamps: false
-    },
-    pool:{
-        max :5,
-        min:0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorAliases: false
-});
- 
+// Extraemos el entorno actual (por defecto 'development')
+const env = process.env.NODE_ENV || 'development';
+
+const db = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        define: {
+            timestamps: false
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    }
+);
+
 export default db;
