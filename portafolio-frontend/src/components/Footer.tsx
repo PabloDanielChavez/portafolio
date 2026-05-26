8b1ae9159c9f2f1ae968164ec0f910af0952e08d
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-05-26 14:22:25 -0300

    Footer HTML y SCSS

--- portafolio-frontend/src/components/Footer.tsx
+++ portafolio-frontend/src/components/Footer.tsx
@@ -2,10 +2,15 @@
 "use client";
 import Link from 'next/link';
 import styles_footer from '../styles/sections/footer.module.scss';
-import { PiMicrosoftOutlookLogo } from 'react-icons/pi';
+import { PiMicrosoftOutlookLogo, PiStack } from 'react-icons/pi';
 import { LiaLinkedin } from 'react-icons/lia';
-import { FaGithub, FaWhatsapp } from 'react-icons/fa';
+import { FaBriefcase, FaEnvelope, FaGithub, FaWhatsapp } from 'react-icons/fa';
 import Image from 'next/image';
+import { BsFillPersonVcardFill } from 'react-icons/bs';
+import { SiCodefactor } from 'react-icons/si';
+import { BiBriefcase } from 'react-icons/bi';
+import { AiTwotoneSchedule } from 'react-icons/ai';
+import { IoIosRocket } from 'react-icons/io';
 
 export default function Header() {
   return (
@@ -13,7 +18,7 @@ export default function Header() {
       <div className={styles_footer.footer_box_logo}>
         <Image 
             className={styles_footer.footer_imagen_logo} 
-            src="/img/Logotipo_Portafolio_PDC/Persona/Pablo.jpg" 
+            src="/img/Logotipo_Portafolio_PDC/Logo/Logo_48x48px.jpg"
             alt="LOGOTIPO"
             width={150}
             height={150}
@@ -25,26 +30,66 @@ export default function Header() {
           <ul className={styles_footer.footer_ul}>
             <li className={styles_footer.footer_li}>
               <a href={`/`} className={styles_footer.footer_a}>
+                <BsFillPersonVcardFill size={20} />
                 <span className={styles_footer.footer_span}>Perfil</span>
               </a>
             </li>
             <li className={styles_footer.footer_li}>
               <a href={`/`} className={styles_footer.footer_a}>
+                <FaBriefcase size={20} />
                 <span className={styles_footer.footer_span}>Experiencia</span>
               </a>
             </li>
             <li className={styles_footer.footer_li}>
               <a href={`/`} className={styles_footer.footer_a}>
+                <SiCodefactor size={20} />
                 <span className={styles_footer.footer_span}>Servicios</span>
               </a>
             </li>
             <li className={styles_footer.footer_li}>
               <a href={`/`} className={styles_footer.footer_a}>
+                <FaEnvelope size={20} />
                 <span className={styles_footer.footer_span}>Contacto</span>
               </a>
             </li>
           </ul>
         </div>
+        <div className={styles_footer.footer_box}>
+          <h2 className={styles_footer.footer_h2}>Secciones</h2>
+          <ul className={styles_footer.footer_ul}>
+            <li className={styles_footer.footer_li}>
+              <a href={`/`} className={styles_footer.footer_a}>
+                <AiTwotoneSchedule size={20} /> 
+                <span className={styles_footer.footer_span}>Clientes</span>
+              </a>
+            </li>
+            <li className={styles_footer.footer_li}>
+              <a href={`/`} className={styles_footer.footer_a}>
+                <BiBriefcase size={20} /> 
+                <span className={styles_footer.footer_span}>Experiencia</span>
+              </a>
+            </li>
+            <li className={styles_footer.footer_li}>
+              <a href={`/`} className={styles_footer.footer_a}>
+                <PiStack size={20} /> 
+                <span className={styles_footer.footer_span}>Habilidades</span>
+              </a>
+            </li>
+            <li className={styles_footer.footer_li}>
+              <a href={`/`} className={styles_footer.footer_a}>
+                <AiTwotoneSchedule size={20} /> 
+                <span className={styles_footer.footer_span}>Servicios</span>
+              </a>
+            </li>
+            <li className={styles_footer.footer_li}>
+              <a href={`/`} className={styles_footer.footer_a}>
+                <IoIosRocket size={20} /> 
+                <span className={styles_footer.footer_span}>Trabajos</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
         <div className={styles_footer.footer_box}>
           <h2 className={styles_footer.footer_h2}>Contacto</h2>
           <ul className={styles_footer.footer_ul}>
