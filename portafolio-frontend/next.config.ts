594cd9d158e4d6f917915e7da17b3f1df2ee021c
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-06-05 05:11:06 -0300

    Estoy cansado jefe

--- portafolio-frontend/next.config.ts
+++ portafolio-frontend/next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from "next";
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({});
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  output: 'export'
+  output: 'export',
+  // Tipamos config como 'any' y especificamos que isServer es un boolean
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      config.resolve.alias['react-dom$'] = 'next/dist/compiled/react-dom';
+      config.resolve.alias['react-dom/client$'] = 'next/dist/compiled/react-dom/client';
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
