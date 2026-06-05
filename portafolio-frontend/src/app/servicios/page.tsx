17a1d2fb5c2151821036fb6566182c71d48507ad
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-06-05 14:09:33 -0300

    Correcion de iteracion

--- portafolio-frontend/src/app/servicios/page.tsx
+++ portafolio-frontend/src/app/servicios/page.tsx
@@ -4,14 +4,14 @@ import Servicios from '@/components/sections/Servicios';
 import Trabajos from '@/components/sections/Trabajos';
 import Clientes from '@/components/sections/Clientes';
 
-export default async function Servicio() {
+export default async function Ser() {
   const data = await getAllPortfolioData();
   if (!data) return <div>Error al cargar</div>;
 
   return (
     <>
       <Servicios servicios={data.Servicios} />
-      <Trabajos trabajos={data.Trabajos} />
+      <Trabajos trabajos={data.Trabajos} showFooter={true}/>
       <Clientes clientes={data.Clientes} />
     </>
   );
