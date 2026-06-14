ce70bcdb3b37f7529d5644603617c0800bccb09f
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-06-14 03:05:45 -0300

    Rendimiento

--- portafolio-frontend/next.config.ts
+++ portafolio-frontend/next.config.ts
@@ -1,16 +1,14 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({});
-
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true, 
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  output: 'export',
-  // Tipamos config como 'any' y especificamos que isServer es un boolean
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+  /**
+   * @param {import('webpack').Configuration} config
+   * @param {{ isServer: boolean }} options
+   */
+  webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.alias['react-dom$'] = 'next/dist/compiled/react-dom';
       config.resolve.alias['react-dom/client$'] = 'next/dist/compiled/react-dom/client';
@@ -18,5 +16,3 @@ const nextConfig = {
     return config;
   },
 };
-
-export default nextConfig;
