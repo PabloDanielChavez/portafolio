570eb7763b2ec0d56a165a3b09e73083039f2dc1
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-06-15 16:55:14 -0300

    GTMdate

--- portafolio-frontend/src/app/layout.tsx
+++ portafolio-frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Script from 'next/script';
 import type { Metadata } from "next";
 import "@/styles/main.scss";
 import GTMTracker from "@/components/sub_components/GTMTracker";
@@ -78,12 +79,20 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="es">
       <head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5L9KF25W');` }} />
+        
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       </head>
       <body style={{ overflowX: "hidden" }}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5L9KF25W"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <header>
           <Header />
         </header>
@@ -93,7 +102,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <footer>
           <Footer />
         </footer>
-        <GTMTracker />
+        {/* <GTMTracker /> */}
       </body>
     </html>
   );
